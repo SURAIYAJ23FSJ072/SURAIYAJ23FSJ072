fa4b17c5b82e132f040de02ab370e0fe9d105f82
SURAIYAJ23FSJ072 <138499875+SURAIYAJ23FSJ072@users.noreply.github.com>
2023-07-27 07:26:11 -0700

    Add files via upload

---
+++ Nestedforloop.java
@@ -0,0 +1,15 @@
+package gtjune;
+
+public class Nestedforloop {
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=3;j<=3;j++)
+			{
+				System.out.println(i+ " " +j);
+		}
+		}
+	}
+
+}
