536498d7ad2a7998c3bf04cf6e8ae27292e61b42
SURAIYAJ23FSJ072 <138499875+SURAIYAJ23FSJ072@users.noreply.github.com>
2023-07-27 06:59:30 -0700

    Add files via upload

---
+++ division.java
@@ -0,0 +1,13 @@
+package gtjune;
+
+public class division {
+public static void main(String[] args)
+{
+	int dividend=25;
+	int divisor=4;
+	int quotient = dividend / divisor;
+	int remainder = dividend % divisor;
+	System.out.println("QUOTIENT = " + quotient);
+	System.out.println("REMAINDER = " + remainder);
+}
+}
