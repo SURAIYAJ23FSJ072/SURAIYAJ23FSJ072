ceb76a2352f451426155235dd65fbd47794e47ed
SURAIYAJ23FSJ072 <138499875+SURAIYAJ23FSJ072@users.noreply.github.com>
2023-07-27 07:08:32 -0700

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,13 @@
+package gtjune;
+
+public class typecasting {
+	public static void main(String[] args) {
+		byte b=10;
+		int i=b;
+		byte d=(byte)i;
+		byte x=10;
+		byte y=20;
+		byte sum=x*y;
+	}
+
+}
