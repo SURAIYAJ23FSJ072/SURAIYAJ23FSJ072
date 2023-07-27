258c5ab6e1589789caa647ee2141276225cd43c9
SURAIYAJ23FSJ072 <138499875+SURAIYAJ23FSJ072@users.noreply.github.com>
2023-07-27 07:00:51 -0700

    Add files via upload

---
+++ closedefaultprimitive.java
@@ -0,0 +1,22 @@
+package gtjune;
+
+public class closedefaultprimitive {
+	   static boolean a;
+	   static double b;
+	   static float c;
+	   static int d;
+	   static long e;
+	   static String f;
+	   public static void main(String[] args)
+	   {
+		   System.out.println("Default values.....");
+	      System.out.println("a = " + a);
+	      System.out.println("b = " + b);
+	      System.out.println("c = " + c);
+	      System.out.println("d = " + d);
+	      System.out.println("e = " + e);
+	      System.out.println("f = " + f);
+
+	   }
+
+}
