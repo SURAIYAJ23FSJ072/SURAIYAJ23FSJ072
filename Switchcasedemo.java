8216d808500a7f531f829d79c736ac9cc1f6ecb7
SURAIYAJ23FSJ072 <138499875+SURAIYAJ23FSJ072@users.noreply.github.com>
2023-07-27 06:56:06 -0700

    Add files via upload

---
+++ Switchcasedemo.java
@@ -0,0 +1,24 @@
+package gtjune;
+
+public class Switchcasedemo {
+public static void main(String[] args)
+{
+	int tech = 1;
+	switch(tech) {
+	case 1:
+	{
+		System.out.println("java");
+		break;
+	}
+	case 2:
+	{
+		System.out.println("ES6");
+		break;
+	}
+	default :
+	{
+		System.out.println("NOT LISTED");
+	}
+	}
+}
+}
