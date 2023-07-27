b999e4059cd50f6e669885d9a363cbf1ceaf6ae9
SURAIYAJ23FSJ072 <138499875+SURAIYAJ23FSJ072@users.noreply.github.com>
2023-07-27 06:33:28 -0700

    Add files via upload

---
+++ ASCIIVALUE.java
@@ -0,0 +1,13 @@
+package gtjune;
+
+public class ASCIIVALUE {
+public static void main(String[] args)
+{
+	char ch='H';
+	int ascii=ch;
+	// you can also cast char to int
+	int castAscii=(int) ch;
+	System.out.println(" THE ASCII VALUE OF " + ch + " IS " + ascii);
+	System.out.println(" THE ASCII VALUE OF " + ch + " IS " + castAscii);
+}
+}
