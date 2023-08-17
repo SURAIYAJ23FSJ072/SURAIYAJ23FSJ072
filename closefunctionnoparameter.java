9c1da9b814a13c191b0d10de99ed30c7694b345b
SURAIYAJ23FSJ072 <138499875+SURAIYAJ23FSJ072@users.noreply.github.com>
2023-08-17 07:51:06 -0700

    Add files via upload

---
+++ closefunctionnoparameter.java
@@ -0,0 +1,15 @@
+package gtjune;
+
+public class closefunctionnoparameter {
+	public static void main()
+	{
+		System.out.println("ADDITION OF TWO NUMBERS");
+		int a=100;
+		int b=500;
+		System.out.println(a+" + "+b);
+		//add two numbers
+		int sum=a+b;
+		System.out.println("THE SUM IS:"+sum);
+	}
+
+}
