9f504dbe8609782808adda5213d90969eb85ac28
SURAIYAJ23FSJ072 <138499875+SURAIYAJ23FSJ072@users.noreply.github.com>
2023-08-17 07:38:46 -0700

    Add files via upload

---
+++ codedifferentclassname.java
@@ -0,0 +1,15 @@
+package gtjune;
+	public class addition {
+	public static void main(String[]args)
+	{
+		System.out.println("ADDITION OF TWO NUMBERS");
+		int a=100;
+		int b=500;
+		System.out.println(a+" + "+b);
+		//add two numbers
+		int sum=a+b;
+		System.out.println("THE SUM IS:"+sum);
+	}
+	}
+
+
